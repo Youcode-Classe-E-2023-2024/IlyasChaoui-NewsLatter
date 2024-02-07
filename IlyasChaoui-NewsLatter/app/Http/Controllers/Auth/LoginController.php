580d7185b6f363965a14b00e453385c35412120e
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-02-07 16:11:32 +0100

    add login function to loginController

---
+++ IlyasChaoui-NewsLatter/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function login(Request $request)
+    {
+        /*
+        Validation
+        */
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+
+        if (!$user) {
+            return back()->withErrors([
+                'email' => 'The provided email does not exist in our records.',
+            ])->onlyInput('email');
+        }
+
+
+        if (!Hash::check($request->password, $user->password)) {
+            return back()->withErrors([
+                'password' => 'The provided password is incorrect.',
+            ])->withInput(['email' => $request->email]);
+        }
+
+
+        if (Auth::attempt($request->only('email', 'password'))) {
+            $request->session()->regenerate();
+            return redirect()->intended('/');
+        }
+
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->withInput($request->except('password'));
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function show()
+    {
+        return view('auth.authentication');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
