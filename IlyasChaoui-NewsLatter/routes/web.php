89bc39b5ddd3ba2604b84c0967a7a65d99200159
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-02-07 16:19:25 +0100

    add routage

--- IlyasChaoui-NewsLatter/routes/web.php
+++ IlyasChaoui-NewsLatter/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/register', [RegisterController::class, 'show'])->name('Auth.register');
+Route::post('/register', [RegisterController::class, 'store'])->name('Auth.registerTrait');
+
+Route::get('/login', [LoginController::class, 'show'])->name('Auth.login');
+Route::post('/login', [LoginController::class, 'login'])->name('Auth.loginTrait');
+
+Route::post('/logout', [LoginController::class, 'destroy'])->middleware('auth');
