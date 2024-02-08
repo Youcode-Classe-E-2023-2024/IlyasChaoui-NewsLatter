fd855d078ca5674ed27a31f346e9643ff6464163
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-02-08 17:14:49 +0100

    add all permissions nedded

---
+++ IlyasChaoui-NewsLatter/database/seeders/PermissionSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Permission::create(['name' => 'create template']);
+        Permission::create(['name' => 'delete template']);
+        Permission::create(['name' => 'send mail']);
+        Permission::create(['name' => 'upload file']);
+        Permission::create(['name' => 'delete file']);
+        Permission::create(['name' => 'assign role']);
+    }
+}
