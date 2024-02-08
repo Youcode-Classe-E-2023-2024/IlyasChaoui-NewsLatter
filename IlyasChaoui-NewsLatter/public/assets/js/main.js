cebf35bb4acf5fae31a789fb98e21a49641b21ae
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-02-08 17:16:34 +0100

    change image path

--- IlyasChaoui-NewsLatter/public/assets/js/main.js
+++ IlyasChaoui-NewsLatter/public/assets/js/main.js
@@ -17,10 +17,10 @@
       // === logo change
       if (ud_header.classList.contains("sticky")) {
         document.querySelector(".header-logo").src =
-          "assets/images/logo/logo.svg"
+          "assets/images/logo/logo.svg.png"
       } else {
         document.querySelector(".header-logo").src =
-          "assets/images/logo/logo-white.svg"
+          "assets/images/logo/logo-white.svg.png"
       }
     }
 
@@ -29,8 +29,8 @@
         // === logo change
         if (ud_header.classList.contains("sticky")) {
           document.querySelector(".header-logo").src =
-            "assets/images/logo/logo-white.svg"
-        } 
+            "assets/images/logo/logo-white.svg.png"
+        }
       }
     }
 
