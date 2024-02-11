5b43711b09e4303090179e3a66893e365aa9c96d
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-02-11 12:44:29 +0100

    and variable emails to pass give access to pass data to the compnenet

---
+++ IlyasChaoui-NewsLatter/app/View/Components/subscribeTable.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class subscribeTable extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public $emails;
+
+    public function __construct($emails)
+    {
+        $this->emails = $emails;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.subscribe-table');
+    }
+}
