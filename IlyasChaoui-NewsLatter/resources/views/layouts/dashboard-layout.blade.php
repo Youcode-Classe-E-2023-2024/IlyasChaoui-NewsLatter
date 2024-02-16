c20212196bed2307b30badeadb3f980f87139eed
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-02-16 15:18:57 +0100

    Update dashboard-layout.blade.php

--- IlyasChaoui-NewsLatter/resources/views/layouts/dashboard-layout.blade.php
+++ IlyasChaoui-NewsLatter/resources/views/layouts/dashboard-layout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png"/>
     <link rel="shortcut icon" href="/assets-home/images/logo/logo-uiverse.png" type="image/x-icon"/>
     <title>Dashboard | @yield('title')</title>
@@ -21,6 +22,7 @@
     <!-- buttons -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
     <link href="./assets/css/button.css" rel="stylesheet"/>
     <!-- Main Styling -->
@@ -70,6 +72,9 @@
 <script src="./assets/js/plugins/perfect-scrollbar.min.js" async></script>
 <!-- main script file  -->
 <script src="./assets/js/argon-dashboard-tailwind.js?v=1.0.1" async></script>
+
+<script src="./assets/js/role.js"></script>
+
 
 
 </html>
