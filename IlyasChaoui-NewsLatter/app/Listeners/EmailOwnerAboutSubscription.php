27aa8e861866dd031355205e7b14773b01cdb277
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-02-16 15:14:53 +0100

    Update EmailOwnerAboutSubscription.php

--- IlyasChaoui-NewsLatter/app/Listeners/EmailOwnerAboutSubscription.php
+++ IlyasChaoui-NewsLatter/app/Listeners/EmailOwnerAboutSubscription.php
@@ -24,6 +24,7 @@ class EmailOwnerAboutSubscription
      */
     public function handle(UserSubscribed $event)
     {
+
         DB::table('emaillists')->insert([
             'email' => $event->email
         ]);
