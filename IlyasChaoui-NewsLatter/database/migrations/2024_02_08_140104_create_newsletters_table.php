c419342d38c8e46605f11d0e34552ddfd5651639
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-02-16 15:16:13 +0100

    Update 2024_02_08_140104_create_newsletters_table.php

--- IlyasChaoui-NewsLatter/database/migrations/2024_02_08_140104_create_newsletters_table.php
+++ IlyasChaoui-NewsLatter/database/migrations/2024_02_08_140104_create_newsletters_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->string('titre');
             $table->string('contenu');
-            $table->date('dateEnvoi');
             $table->enum('deleted', ['restaured'])->default('restaured');
             $table->timestamps();
             $table->unsignedBigInteger('user_id');
